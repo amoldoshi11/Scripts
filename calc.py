da55c369dac7e34022ec5bd16bce322fb2d63281
Amol Doshi <amoldoshi11@gmail.com>
2020-03-01 12:43:36 +0000

    Added calc.py and worked on mulitiply Function

---
+++ calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def add(x,y): 
+
+    return(x+y)
+
+def subtract(x,y): 
+
+    pass 
+
+def multiply(x,y): 
+
+    pass 
+
+def divide(x,y): 
+
+    pass 
+
+def square(x,y): 
+
+    pass 
